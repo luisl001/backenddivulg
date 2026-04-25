62fc411f196c09990d8cc1b73b1d1692218b99b3
luisl001 <luisgodoy0100@gmail.com>
2026-04-25 17:14:30 -0600

    updated code

--- db.js
+++ db.js
@@ -9,7 +9,12 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
+  waitForConnections: true,  
+  port: process.env.DB_PORT || 27736,
+  ssl: {
+    // Esto es vital para Aiven
+    rejectUnauthorized: true 
+  },
   connectionLimit: 10,
   queueLimit: 0
 });
